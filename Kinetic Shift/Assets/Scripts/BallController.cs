048421df86cf44a238a30fcea10e9c2eb4b6c23b
NickWain <9992057@student.swin.edu.au>
2015-08-21 12:28:13 +1000

    Started implementing player controls

---
+++ Kinetic Shift/Assets/Scripts/BallController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		if (Input.GetKey(KeyCode.LeftArrow)) {
+			transform.Translate(Vector2.left * Time.deltaTime);
+		}
+		
+		if (Input.GetKey(KeyCode.RightArrow)) {
+			transform.Translate(Vector2.right * Time.deltaTime);
+		}
+	}
+}
