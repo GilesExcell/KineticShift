a0f90f8543754f5617f24c29994603b0d4a861d4
Giles Excell <9713395@student.swin.edu.au>
2015-08-21 12:08:11 +1000

    Input Framework

---
+++ Kinetic Shift/Assets/Scripts/PlayerActions.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+using InControl;
+
+public class PlayerActions : PlayerActionSet {
+	public PlayerAction Left;
+	public PlayerAction Right;
+	public PlayerAction Jump;
+	public PlayerAction Shift;
+	public PlayerOneAxisAction Move;
+
+	public PlayerActions()
+	{
+		Left = CreatePlayerAction( "Move Left" );
+		Right = CreatePlayerAction( "Move Right" );
+		Jump = CreatePlayerAction( "Jump" );
+		Shift = CreatePlayerAction("Kinetic Shift"):
+		Move = CreateOneAxisPlayerAction (Left, Right);
+	}
+
+	public static PlayerActions CreateWithDefaultBindings()
+	{
+		var playerActions = new PlayerActions();
+
+		playerActions.Jump.AddDefaultBinding( Key.Space );
+		playerActions.Jump.AddDefaultBinding( InputControlType.Action1 );
+		
+		playerActions.Left.AddDefaultBinding( Key.LeftArrow );
+		playerActions.Right.AddDefaultBinding( Key.RightArrow );
+
+		playerActions.Left.AddDefaultBinding( InputControlType.LeftStickLeft );
+		playerActions.Right.AddDefaultBinding( InputControlType.LeftStickRight );
+
+		playerActions.Left.AddDefaultBinding( InputControlType.DPadLeft );
+		playerActions.Right.AddDefaultBinding( InputControlType.DPadRight );
+
+		playerActions.Shift.AddDefaultBinding (Key.Shift);
+		playerActions.Shift.AddDefaultBinding (InputControlType.Action2);
+
+		return playerActions;
+	}
+}
---
+++ Kinetic Shift/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using InControl;
+
+public class PlayerController : MonoBehaviour {
+	PlayerActions playerActions;
+
+	// Use this for initialization
+	void Start () {
+		playerActions = PlayerActions.CreateWithDefaultBindings();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (playerActions.Jump.WasPressed)
+		{
+			Jump();
+		}
+
+		Move (playerActions.Move.Value, playerActions.Shift.IsPressed);
+	}
+
+	void Jump() {
+
+	};
+
+	void Move(float x, bool shift) {
+
+	};
+}
