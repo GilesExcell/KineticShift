c6cac5c819d8742ed3d7fef9978172bed769813a
NickWain <9992057@student.swin.edu.au>
2015-08-21 12:32:51 +1000

    Changed movement slightly

--- Kinetic Shift/Assets/Scripts/BallController.cs
+++ Kinetic Shift/Assets/Scripts/BallController.cs
@@ -3,6 +3,9 @@ using System.Collections;
 
 public class BallController : MonoBehaviour {
 
+	int maxSpeed = 20;
+	int accel = 10;
+
 	// Use this for initialization
 	void Start () {
 		
@@ -12,11 +15,11 @@ public class BallController : MonoBehaviour {
 	void Update () {
 		
 		if (Input.GetKey(KeyCode.LeftArrow)) {
-			transform.Translate(Vector2.left * Time.deltaTime);
+			transform.Translate(Vector2.left * Time.deltaTime * accel);
 		}
 		
 		if (Input.GetKey(KeyCode.RightArrow)) {
-			transform.Translate(Vector2.right * Time.deltaTime);
+			transform.Translate(Vector2.right * Time.deltaTime * accel);
 		}
 	}
 }
