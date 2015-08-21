49c3f9b8588757e32f168db1710191cf04fca49a
Giles Excell <9713395@student.swin.edu.au>
2015-08-21 12:28:25 +1000

    SpringController input

---
+++ Kinetic Shift/Assets/Scripts/SpringActions.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+using InControl;
+
+public class SpringActions : PlayerActionSet {
+	public PlayerAction Left;
+	public PlayerAction Right;
+	public PlayerAction Up;
+	public PlayerAction Down;
+	public PlayerAction Shift;
+
+	public SpringActions()
+	{
+		Left = CreatePlayerAction( "Left" );
+		Right = CreatePlayerAction( "Right" );
+		Up = CreatePlayerAction( "Up" );
+		Down = CreatePlayerAction ("Down");
+		Shift = CreatePlayerAction ("Kinetic Shift");
+	}
+	
+	public static SpringActions CreateWithDefaultBindings()
+	{
+		var playerActions = new SpringActions();
+		
+		playerActions.Left.AddDefaultBinding( Key.LeftArrow );
+		playerActions.Right.AddDefaultBinding( Key.RightArrow );
+		playerActions.Up.AddDefaultBinding( Key.UpArrow );
+		playerActions.Down.AddDefaultBinding( Key.DownArrow );
+
+		playerActions.Left.AddDefaultBinding( InputControlType.LeftStickLeft );
+		playerActions.Right.AddDefaultBinding( InputControlType.LeftStickRight );
+		playerActions.Up.AddDefaultBinding( InputControlType.LeftStickUp );
+		playerActions.Down.AddDefaultBinding( InputControlType.LeftStickDown );
+
+		playerActions.Left.AddDefaultBinding( InputControlType.DPadLeft );
+		playerActions.Right.AddDefaultBinding( InputControlType.DPadRight );
+		playerActions.Up.AddDefaultBinding( InputControlType.DPadUp );
+		playerActions.Down.AddDefaultBinding( InputControlType.DPadDown );
+
+		playerActions.Shift.AddDefaultBinding (Key.Shift);
+		playerActions.Shift.AddDefaultBinding (InputControlType.Action1);
+
+
+		return playerActions;
+	}
+}
---
+++ Kinetic Shift/Assets/Scripts/SpringController.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+using InControl;
+
+public class SpringController : MonoBehaviour {
+	SpringActions playerActions;
+	
+	// Use this for initialization
+	void Start () {
+		playerActions = SpringActions.CreateWithDefaultBindings();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (playerActions.Up.WasPressed)
+		{
+			Up(playerActions.Shift.IsPressed);
+		}
+		if (playerActions.Down.WasPressed)
+		{
+			Down(playerActions.Shift.IsPressed);
+		}
+		if (playerActions.Left.WasPressed)
+		{
+			Left(playerActions.Shift.IsPressed);
+		}
+		if (playerActions.Right.WasPressed)
+		{
+			Right(playerActions.Shift.IsPressed);
+		}
+
+
+	}
+	
+	void Up(bool shift) {
+		// pop top panel
+	}
+	
+	void Down(bool shift) {
+		// pop down panel
+	}
+
+	void Left(bool shift) {
+		// pop left panel
+	}
+
+	void Right(bool shift) {
+		// pop right panel
+	}
+}
