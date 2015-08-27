fab66f4a1810d446578546d6d06f6c7d3e5a6e28
chloemitterer <9725725@student.swin.edu.au>
2015-08-27 18:03:02 +1000

    movement
    basic left/right movement started

---
+++ Kinetic Shift/Assets/Scripts/CircleController.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CircleController : MonoBehaviour {
+
+	public float maxTorque = 1000f;
+	float move;
+
+	PlayerActions playerActions;
+	
+	// Use this for initialization
+	void Start () {
+		playerActions = PlayerActions.CreateWithDefaultBindings();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (playerActions.Jump.WasPressed)
+		{
+			//Jump(playerActions.Shift.IsPressed);
+		}
+		
+		Move (playerActions.Move.Value, playerActions.Shift.IsPressed);
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+
+		GetComponent<Rigidbody2D>().AddTorque (move * maxTorque);
+	}
+
+	void Move(float x, bool shift) {
+		move = -x;
+		Debug.Log (move);
+	}
+
+}
