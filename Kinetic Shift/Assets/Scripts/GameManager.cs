1589aaffe97fb64783a69805a69d7d67a42a4182
Kraminka <nickwain1@hotmail.com>
2015-09-07 00:20:36 +1000

    Basic Score Display

--- Kinetic Shift/Assets/Scripts/GameManager.cs
+++ Kinetic Shift/Assets/Scripts/GameManager.cs
@@ -1,29 +1,40 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class GameManager : MonoBehaviour {
 
 	public int score { get; private set; }
 	public float currentTime { get; private set; }
+	public Text scoreText;
 
 	// Use this for initialization
 	void Start () {
 		score = 0;
 		currentTime = 0;
+		SetScoreText();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		currentTime += Time.deltaTime;
+
+		// Display Score
+
 	}
 
 	void AddPoints(int points) {
 		score += points;
+		SetScoreText();
 	}
 
 	void RemovePoints(int points) {
 		score -= points;
 		if (score < 0)
 			score = 0;
+	}
+
+	void SetScoreText (){
+		scoreText.text = "Score: " + score.ToString();
 	}
 }
