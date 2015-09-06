9d59be34ea9831d91ab885d4c4c3737fbe2d8e11
GilesExcell <9713395@student.swin.edu.au>
2015-09-07 08:16:52 +1000

    Testlevel, camerafollow

---
+++ Kinetic Shift/Assets/CameraFollow.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour {
+	public Transform target = null;
+	public float highSpeed = 10;
+	public float lowSpeed = 1;
+	public float maxDistance;
+
+	// Use this for initialization
+	void Start () {
+		if (target == null) {
+			target = GameObject.Find("Player").transform;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float distance = (transform.position - target.position).magnitude;
+
+
+		transform.position = Vector2.Lerp (transform.position, target.position, Time.deltaTime);
+
+	}
+}
--- Kinetic Shift/Assets/Scripts/CircleController.cs
+++ Kinetic Shift/Assets/Scripts/CircleController.cs
@@ -5,9 +5,17 @@ using System.Collections.Generic;
 public class CircleController : MonoBehaviour {
 
 	public float maxTorque = 1f;
-	public float jumpForce = 1f;
+	public float jumpForce = 5f;
+	public float jumpTime = 0.1f;
+	public float drainRate = 0.5f;
 	float move;
 	bool grounded = false;
+	float jump = 0.0f;
+	Vector2 direction;
+	Rigidbody2D body;
+	float shiftImpulse;
+	public float storedEnergy { get; private set; }
+	Vector2 lastVelocity = Vector2.zero;
 
 	int currentCollisions;
 
@@ -15,6 +23,7 @@ public class CircleController : MonoBehaviour {
 	
 	// Use this for initialization
 	void Start () {
+		body = GetComponent<Rigidbody2D> ();
 		playerActions = BallActions.CreateWithDefaultBindings();
 	}
 	
@@ -22,24 +31,46 @@ public class CircleController : MonoBehaviour {
 	void Update () {
 		if (playerActions.Jump.WasPressed)
 		{
-			Jump(playerActions.JumpDirection.Value, playerActions.Shift.IsPressed);
+			Jump(playerActions.JumpDirection.Value);
 		}
 		if (playerActions.Brake.IsPressed){
 			Brake();
 		}else{
-		Move (playerActions.Move.Value, playerActions.Shift.IsPressed);
+			Move (playerActions.Move.Value, playerActions.Shift.IsPressed);
 		}
 	}
 	
 	// Update is called once per frame
 	void FixedUpdate () {
+		if (body.velocity.sqrMagnitude < lastVelocity.sqrMagnitude) {
+			storedEnergy += body.mass * (body.velocity - lastVelocity).sqrMagnitude / 2.0f;
+		}
+		Debug.Log (storedEnergy, gameObject);
 
-		GetComponent<Rigidbody2D>().AddTorque (move * maxTorque);
-		Debug.Log (currentCollisions);
+
+		body.AddTorque (move * maxTorque);
 		if (currentCollisions == 0) {
 			grounded = false;
 		} else {
 			grounded = true;
+		}
+
+		if (grounded && jump > 0) {
+			jump = 0.0f;
+			shiftImpulse = jumpForce + Mathf.Sqrt(storedEnergy);
+			storedEnergy = 0;
+			body.AddForce(direction * shiftImpulse, ForceMode2D.Impulse);
+		}
+
+		jump -= Time.deltaTime;
+		lastVelocity = body.velocity;
+
+		if (storedEnergy > 0) {
+			storedEnergy *= (1 - Time.deltaTime * drainRate);
+		}
+
+		if (storedEnergy < 0) {
+			storedEnergy = 0;
 		}
 	}
 
@@ -52,9 +83,9 @@ public class CircleController : MonoBehaviour {
 
 	}
 
-	void Jump(Vector2 direction, bool shift) {
-		if (grounded)
-			GetComponent<Rigidbody2D> ().AddForce (direction * jumpForce, ForceMode2D.Impulse);
+	void Jump(Vector2 dir) {
+		jump = jumpTime;
+		direction = dir.normalized;
 	}
 
 	void OnCollisionEnter2D(){
