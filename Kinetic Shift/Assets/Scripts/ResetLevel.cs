3d18147a19bfd29de92d9a8eec02d99a4b06b4eb
Csilla Mitterer <9725725@student.swin.edu.au>
2015-09-07 10:25:02 +1000

    reset level button

---
+++ Kinetic Shift/Assets/Scripts/ResetLevel.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using InControl;
+
+public class ResetLevel : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		InputDevice device = InputManager.ActiveDevice;
+		if (device.MenuWasPressed) {
+			Application.LoadLevel (Application.loadedLevelName);
+		}
+	}
+}
