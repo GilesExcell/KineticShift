5625aebe8854da3f94c486ac9ad3055985259be6
NickWain <9992057@student.swin.edu.au>
2015-09-11 11:14:19 +1000

    Score and energy bar added

--- Kinetic Shift/Assets/Scripts/GameManager.cs
+++ Kinetic Shift/Assets/Scripts/GameManager.cs
@@ -1,15 +1,18 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class GameManager : MonoBehaviour {
 
 	public int score { get; private set; }
 	public float currentTime { get; private set; }
+	public Text scoreText;
 
 	// Use this for initialization
 	void Start () {
 		score = 0;
 		currentTime = 0;
+		setScoreText ();
 	}
 	
 	// Update is called once per frame
@@ -19,11 +22,18 @@ public class GameManager : MonoBehaviour {
 
 	void AddPoints(int points) {
 		score += points;
+		setScoreText ();
 	}
 
 	void RemovePoints(int points) {
 		score -= points;
 		if (score < 0)
 			score = 0;
+
+		setScoreText ();
+	}
+
+	void setScoreText(){
+		scoreText.text = "Score: " + score.ToString ();
 	}
 }
