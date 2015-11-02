aab1a743f3d1ab66e12a1f219240928d2fc026fa
Kraminka <nickwain1@hotmail.com>
2015-11-03 06:37:03 +1100

    Score and Time displayed in level completion screen

--- Kinetic Shift/Assets/Scripts/GameManager.cs
+++ Kinetic Shift/Assets/Scripts/GameManager.cs
@@ -6,12 +6,14 @@ public class GameManager : MonoBehaviour {
 
 	public int score { get; private set; }
 	public float currentTime { get; private set; }
+	public Text timerText;
 	public GameObject player;
 	private CircleController playerController;
 	public Text scoreText;
 	public Slider energySlider;
-	
+	public Text Results;
 	public Canvas lvlCompScreen;
+
 	public bool lvlEnd; // Player completed level
 
 	// Use this for initialization
@@ -36,10 +38,18 @@ public class GameManager : MonoBehaviour {
 
 		UpdateKESlider();
 
+		// Has the player finished the level?
 		if (lvlEnd)
 		{
+			// Set score and time results
+			Results.text = ((int)currentTime).ToString () + " Seconds" +" | " + score.ToString () + " Pts";
+
+			// Display screen with points and lvl select button
 			lvlCompScreen.enabled = true;
+			lvlEnd = false; // Prevent score and time from updating
 		}
+
+		timerText.text = "Time: " + ((int)currentTime).ToString () + "s";
 	}
 
 	// Add points to the player's score
